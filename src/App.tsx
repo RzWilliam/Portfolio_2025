3c8612e7b07a7cced9b75be3af0a058713643e34
RzWilliam <williamrodriguez0621@gmail.com>
2025-10-05 00:36:23 +0200

    🎨 Add sonner to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Main from './components/Main';
 import Logo from './assets/logo_white.png';
+import { Toaster } from 'sonner';
 
 function App() {
   return (
     <>
       <img src={Logo} alt="Logo" className="absolute top-4 left-4 w-16" />
+      <Toaster position="bottom-center" />
       <Main />
     </>
   );
