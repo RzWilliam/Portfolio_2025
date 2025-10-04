4315654481d13488f1a26b860797f90e1770c972
RzWilliam <williamrodriguez0621@gmail.com>
2025-10-05 00:41:41 +0200

    🔥 Remove logo top-left

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Main from './components/Main';
-import Logo from './assets/logo_white.png';
 import { Toaster } from 'sonner';
 
 function App() {
   return (
     <>
-      <img src={Logo} alt="Logo" className="absolute top-4 left-4 w-16" />
       <Toaster position="bottom-center" />
       <Main />
     </>
